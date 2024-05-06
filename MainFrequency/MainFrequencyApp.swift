0a5fb9af18de0219d17371a620c1e3aac3b61d81
Joaquim Pessôa Filho <joaquimp@gmail.com>
2024-05-06 12:54:52 -0300

    Initial commit

---
+++ MainFrequency/MainFrequencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainFrequencyApp.swift
+//  MainFrequency
+//
+//  Created by Joaquim Pessoa Filho on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MainFrequencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
